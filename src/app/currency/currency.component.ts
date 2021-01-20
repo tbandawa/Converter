b03784ef57887bd248d08a49972e0f9f341985db
tee <tonderaibandawa@gmail.com>
2021-01-20 21:11:13 +0200

    two way conversition for currency

--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -9,8 +9,6 @@ import { Currency, Rate, RateService } from '../core'
 export class CurrencyComponent  {
 
   currencies: Currency[] = []
-  currencyType: string
-  output: number
   rate: Rate
 
   selectCurrencyOne: string
@@ -18,21 +16,16 @@ export class CurrencyComponent  {
   selectCurrencyTwo: string
   inputCurrencyTwo: number
 
-
   constructor(private rateService: RateService) { 
     this.rateService.getRates().subscribe({
       next: data => {
-
         this.rate = data
-
         this.rate.rates[data.base] = 1
-
         for (var key in data.rates) {
           if (data.rates.hasOwnProperty(key)) {
               this.currencies.push({type: key, rate: data.rates[key]})
           }
         }
-
       },
       error: error => {
         console.log(error)
@@ -40,35 +33,32 @@ export class CurrencyComponent  {
     })
   }
 
-  onCurrencyOneSelection(value: string) {
-      console.log(value)
+  onCurrencyOneSelection() {
+    this.onCurrencyTwoInput() 
   }
 
-  onCurrencyTwoSelection(value: string) {
-    console.log(value)
+  onCurrencyTwoSelection() {
+    this.onCurrencyOneInput()
   }
 
   onCurrencyOneInput() {
-    console.log(this.inputCurrencyOne)
+    if(this.inputCurrencyOne != null && this.selectCurrencyOne != null && this.selectCurrencyTwo != null) {
+      var inputCurrency = this.rate.rates[this.selectCurrencyOne]
+      var outputCurrency = this.rate.rates[this.selectCurrencyTwo]
+      this.inputCurrencyTwo = this.convertRates(inputCurrency, outputCurrency, this.inputCurrencyOne)
+    }
   }
 
-onCurrencyTwoInput() {
-  console.log(this.inputCurrencyTwo)
-}
+  onCurrencyTwoInput() {
+    if(this.inputCurrencyTwo != null && this.selectCurrencyOne != null && this.selectCurrencyTwo != null) {
+      var inputCurrency = this.rate.rates[this.selectCurrencyOne]
+      var outputCurrency = this.rate.rates[this.selectCurrencyTwo]
+      this.inputCurrencyOne = this.convertRates(outputCurrency, inputCurrency, this.inputCurrencyTwo)
+    }
+  }
 
   convertRates(inRate: number, outRate: number, value: number) : number {
     return ((value/inRate)*outRate)
   }
-
-  /*submit() {
-    if (!this.convertForm.valid) {
-      return;
-    }
-    var fromCurrency = this.convertForm.value.fromCurrency
-    var toCurrency = this.convertForm.value.toCurrency
-    this.currencyType = toCurrency
-    var valueToConvert = this.convertForm.value.inputValue
-    this.output = this.convertRates(this.rate.rates[fromCurrency], this.rate.rates[toCurrency], valueToConvert)
-  }*/
 
 }
